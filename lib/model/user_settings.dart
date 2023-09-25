0079066fe7ea9dff25939cd426596b8d3b87326e
MajaL1 <MajaL1@users.noreply.github.com>
2023-09-25 12:24:11 +0200

    Update user_settings.dart

--- lib/model/user_settings.dart
+++ lib/model/user_settings.dart
@@ -1,1 +1,15 @@
+class UserSettings {
+  String id;
+ 
+  UserSettings({required this.id});
 
+  Map<String, dynamic> toJson() {
+    return {"id": id};
+  }
+
+  factory UserSettings.fromJson(Map<String, dynamic> json) {
+    return UserSettings(
+        id: json["id"]
+    );
+  }
+}
