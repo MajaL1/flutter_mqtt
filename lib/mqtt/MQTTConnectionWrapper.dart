af745546f51d166e24c165ae664d706d037ddae8
Majalok <majalokar@gmail.com>
2023-11-03 17:12:46 +0100

    create new class MqttConnectionWrapper, preparing to refactor (move code to main...)

--- lib/mqtt/MQTTConnectionWrapper.dart
+++ lib/mqtt/MQTTConnectionWrapper.dart
@@ -1,37 +1,39 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_test/mqtt/state/MQTTAppState.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../model/user.dart';
 import '../model/user_data_settings.dart';
+import '../util/mqtt_connect_util.dart';
 
-class MQTTConnectionManager {
+class MQTTConnectionWrapper {
   // Private instance of client
   final MQTTAppState _currentState;
- static MqttServerClient ?client;
+  static MqttServerClient ?client;
   final String _identifier;
   final String _host;
   final String _topic1;
   final String _topic2;
 
 
-
   // Constructor
   // ignore: sort_constructors_first
-  MQTTConnectionManager(
-      {required String host,
-        required String topic1,
-        required String topic2,
-        required String identifier,
-        required MQTTAppState state})
+  MQTTConnectionWrapper({required String host,
+    required String topic1,
+    required String topic2,
+    required String identifier,
+    required MQTTAppState state})
       : _identifier = identifier,
         _host = host,
         _topic1 = topic1,
         _topic2 = topic2,
-      _currentState = state;
+        _currentState = state;
 
 
   void initializeMQTTClient() {
@@ -55,7 +57,6 @@ class MQTTConnectionManager {
         .withWillQos(MqttQos.atLeastOnce);
     print('EXAMPLE:: client connecting....');
     client!.connectionMessage = connMess;
-
   }
 
   // Connect to the host
@@ -63,7 +64,7 @@ class MQTTConnectionManager {
   Future<void> connect() async {
     assert(client != null);
     try {
-      String username= "test1";
+      String username = "test1";
       String password = "MDQ0MThmZmM1NTI4OGQ4OQ==";
       print('::Navis app client connecting....');
       _currentState.setAppConnectionState(MQTTAppConnectionState.connecting);
@@ -129,27 +130,97 @@ class MQTTConnectionManager {
 
       SharedPreferences preferences = await SharedPreferences.getInstance();
       /***  polnjenje objekta - data ***/
-      if(topicName!.contains("settings")){
+      if (topicName!.contains("settings")) {
         debugPrint("___________________________________________________");
         debugPrint("from which topic $topicName");
         debugPrint("__________ $decodeMessage");
         debugPrint("___________________________________________________");
         preferences.setString("settings_mqtt", decodeMessage);
       }
-      if(topicName!.contains("data")){
+      if (topicName!.contains("data")) {
         preferences.setString("data_mqtt", decodeMessage);
       }
-      if(topicName!.contains("alarm")){
+      if (topicName!.contains("alarm")) {
         preferences.setString("alarm_mqtt", decodeMessage);
       }
 
-     // preferences.setString("settings_mqtt", decodeMessage);
+      // preferences.setString("settings_mqtt", decodeMessage);
       print("======= pt: ${pt} , topic: $_topic1, $_topic2");
       print(
-          'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+          'EXAMPLE::Change notification:: topic is <${c[0]
+              .topic}>, payload is <-- $pt -->');
       print('');
-      });
+    });
     print(
         'EXAMPLE::OnConnected client callback - Client connection was sucessful');
   }
+
+  /*Future<void> connectToBroker(List<String> brokerAddressList) async {
+    for (var brokerAddress in brokerAddressList) {
+      if (brokerAddress.contains('/alarm')) {} else
+      if (brokerAddress.contains('/settings')) {} else
+      if (brokerAddress.contains('/data')) {}
+      debugPrint("brokerAddress: $brokerAddress");
+    }
+    if (MQTTAppConnectionState.disconnected ==
+        currentAppState?.getAppConnectionState) {
+      await configureAndConnect(currentAppState);
+    } */
+  }
+
+  /**********  Premakni v main.dart ******************/
+  // Initalize user data and connect
+  Future<User> initalizeConnection(currentAppState) async {
+    debugPrint("calling initalizeConnection in main.dart");
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    const storage = FlutterSecureStorage();
+
+    // ***************** connect to broker ****************
+    User user = await MqttConnectUtil.readUserData();
+    MqttConnectUtil.initalizeUserPrefs(user);
+    List<String> brokerAddressList = MqttConnectUtil.getBrokerAddressList(user);
+    //connectToBroker(brokerAddressList);
+
+    configureAndConnect(currentAppState);
+    // *****************************************************
+
+    debugPrint("preferences ${sharedPreferences.toString()}");
+    await storage.write(key: 'jwt', value: 'jwtTokenTest');
+//storage.containsKey(key: "jwt")
+    String? readToken = await storage.read(key: "token");
+    debugPrint("token from flutter secure storage: $readToken");
+    return user;
+  }
+
+  // Connect to brokers
+  Future<void> configureAndConnect(currentAppState) async {
+    // TODO: Use UUID
+    String osPrefix = 'Flutter_iOS';
+    if (Platform.isAndroid) {
+      osPrefix = 'Flutter_Android';
+    }
+    manager = MQTTConnectionManager(
+        host: 'test.navis-livedata.com',
+        //_hostTextController.text,
+        topic1: 'c45bbe821261/settings'
+            '',
+        //_topicTextController.text,
+        // topic2:
+        topic2: 'c45bbe821261/data',
+        identifier: osPrefix,
+        state: currentAppState);
+    //manager.initializeMQTTClient();
+    //await manager.connect();
+
+
+    // pridobivanje najprej settingov, samo za topic (naprave) -dodaj v objekt UserSettings
+    if (MQTTAppConnectionState.connected ==
+        currentAppState?.getAppConnectionState) {
+      //MQTTConnectionManager._publishMessage(topic, text);
+      String? t = await currentAppState?.getHistoryText;
+
+      debugPrint("****************** $t");
+    }
+ /**************** end premakni v main.dart ******************/
 }
