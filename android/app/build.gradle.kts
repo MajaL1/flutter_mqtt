e91931d60b7e4e56846a783d93bc87313ce3b142
Majalok <majalokar@gmail.com>
2025-10-28 12:59:37 +0100

    changed build app, added config for keystore (build.gradle)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,14 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")  // no version here
@@ -25,13 +36,39 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
-}
 
-flutter {
-    source = "../.."
+
+
+    signingConfigs {
+        create("release") {
+            val storeFilePath = keystoreProperties["storeFile"] as String?
+            if (storeFilePath != null) {
+                storeFile = file(storeFilePath)
+            } else {
+                println("⚠️ storeFile is null — check your key.properties path!")
+            }
+            storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+
+
+        }
+    }
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    }
+
+    flutter {
+        source = "../.."
+    }
 }
